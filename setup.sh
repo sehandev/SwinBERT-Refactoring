8fe949ba5e889aa32392267bf6e5c42e10c2cd3c
liveseongho <liveseongho@gmail.com>
2022-11-03 15:13:48 +0900

    Docker Compose

--- setup.sh
+++ setup.sh
@@ -2,6 +2,4 @@ ln -s /evalcap/coco_caption /videocap/src/evalcap/coco_caption
 ln -s /evalcap/cider /videocap/src/evalcap/cider
 pip install fvcore ete3 transformers
 pip install --upgrade azureml-core
-df -h
-ls -al
 export TORCH_HOME=/models
